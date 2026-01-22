5cabd1e9865707d1c7de4d1bac20d1e76c6debdf
Martin Rodriguez <martinrodriguez@MacBook-Pro-de-Martin.local>
2026-01-22 12:19:28 -0600

    feat: (update) support macos tahoe 26

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,6 +11,7 @@ let package = Package(
         .macOS("13.0"),
         .watchOS("9.0"),
         .tvOS("16.0"),
+        .visionOS("1.0"),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -29,7 +30,7 @@ let package = Package(
         
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
         
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.1"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
